f97d8da79ae0e97b96bd9444be1a023da8e4dc1f
Mark Timmings <mark.timmings@mtbsoftware.co.uk>
2025-05-18 16:14:37 +0100

    Updated version

---
+++ PutridParrot.Har.Tests/InitializationTests.cs
@@ -0,0 +1,34 @@
+﻿namespace PutridParrot.Har.Tests;
+
+public class InitializationTests
+{
+    [Test]
+    public void EnsureRequiredPropertiesAreSet()
+    {
+        var har = new Har
+        {
+            Log = new Log
+            {
+                Creator = new Creator
+                {
+                    Name = "PutridParrot.Har",
+                    Version = "0.0.1"
+                },
+                Browser = new Browser
+                {
+                    Name = "PutridParrot.Har",
+                    Version = "0.0.1"
+                },
+                Entries = [],
+                Version = "1.2"
+            }
+        };
+        
+        var client = new HarClient();
+        var serialized = client.Serialize(har);
+        var deserialized = client.Deserialize(serialized);
+
+        Assert.That(deserialized, Is.Not.Null);
+        Assert.That(client.Serialize(deserialized), Is.EqualTo(serialized));
+    }
+}
